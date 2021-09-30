972a8714b56bd619afb60d3225a51b98a0641964
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-30 16:33:06 +0530

    Update Sample.js

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,15 +1,55 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../App.css';
 
 const Sample = (props) => {
 
-    // 
+    const [emp, setEmp] = useState(
+        {
+            firstName: "Monu",
+            salary: 10000,
+            phones: {
+                officePhone: 23456789,
+                mobile: 9876543210
+            }
+        }
+    );
 
+    const handleEmpData = (evt) => {
+        console.log("handleEmpData", evt.target.name, evt.target.value);
+        setEmp({
+            ...emp,
+            [evt.target.name]: evt.target.value
+        });
+    }
+
+    const submitEmpData = (evt) => {
+        console.log("submitEmpData", evt.target.name, evt.target.value);
+        setEmp({
+            ...emp,
+            [evt.target.name]: evt.target.value
+        });
+        evt.preventDefault();
+    }
 
     return (
         <div className="App">
             <h1>Sample Component</h1>
             <p>This is sample component.</p>
+            <div>
+                <form onSubmit={submitEmpData} >
+                    <input
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        value={emp.firstName}
+                        onChange={handleEmpData}
+                    />
+                    <button type="submit" onClick={submitEmpData}>Emp Data</button>
+                </form>
+            </div>
+            <p> {emp.firstName} </p>
+            <p> {emp.phones.mobile} </p>
             <p> {props.firstName} </p>
             <p> {props.salary} </p>
         </div>
