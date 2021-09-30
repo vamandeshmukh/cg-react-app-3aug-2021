95c49488c23514cb4676a7ac10ecb5e83ea9c2e8
vamandeshmukh <vamandeshmukh@gmail.com>
2021-09-30 13:34:27 +0530

    Update Sample.js

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import '../App.css';
 
+class Sample extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render = () => {
+        return (
+            <div className="App">
+                <h1>Sample Component</h1>
+                <p>This is sample component.</p>
+                <p>{new Date().toLocaleDateString('en-IN')}</p>
+                <p>{new Date().toLocaleDateString('mr-IN')}</p>
+            </div>
+        );
+    }
+}
+
+export default Sample;
+
 // Types of components in React 
 // 1. function components 
 // 2. class components 
+
+// 2.  example of class component 
 
 // 1. example of function component 
 // const Sample = () => {
@@ -16,21 +38,3 @@ import '../App.css';
 // }
 // export default Sample;
 
-// 2.  example of class component 
-class Sample extends React.Component {
-
-    constructor() {
-        super();
-    }
-
-    render = () => {
-        return (
-            <div className="App">
-                <h1>Sample Component</h1>
-                <p>This is sample component.</p>
-            </div>
-        );
-    }
-}
-
-export default Sample;
