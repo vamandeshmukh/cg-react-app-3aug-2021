dc44ff8eb95424bbac48ef2af032a5ba7b50ceb1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-01 16:18:59 +0530

    Commit
    Commit

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 
 const Child = (props) => {
-    const [childData, setChildData] = useState({});
+
+    const [childEmp, setChildEmp] = useState({}); // child state   
 
     useEffect(() => {
-        setChildData(props.parentData);
+        setChildEmp({
+            name: 'Tonu',
+            salary: 30.5
+        }
+        );
     }, []);
+
+    const onTrigger = () => {
+        
+        props.parentCallback(childEmp);
+    };
 
     return (
         <div className="container">
             <h1 className="display-4 text-primary">Child Component</h1>
             <p>Data in Child component</p>
+            <p> Existing Data in Child {childEmp.name}</p>
+            <button onClick={onTrigger}>Pass childEmp Data to Parent</button>
         </div>
     );
 }
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,21 +2,33 @@ import React, { useEffect, useState } from 'react';
 import Child from './Child';
 
 const Parent = () => {
-    const [parentData, setParentData] = useState({});
+    const [parentEmp, setParentEmp] = useState({}); // parent state  
+    const [childEmp, setChildEmp] = useState({}); // from callback 
 
     useEffect(() => {
-        setParentData({
-            firstName: "Monu",
-            salary: 10000,
-        });
+
+        setParentEmp({
+            name: 'Monu',
+            salary: 20.5
+        }
+        );
     }, []);
+
+    const handleCallback = (childEmpData) => {
+        setChildEmp(childEmpData);
+        console.log(childEmpData);
+    }
+
 
     return (
         <div className="container">
             <h1 className="display-4 text-primary">Parent Component</h1>
             <p>Data in Parent component</p>
-            <p> {parentData.firstName} </p>
-            <Child />
+            <p> Existing Data in Parent {parentEmp.name}</p>
+            <p>Data coming from Child to Parent {childEmp.name}</p>
+            <Child
+                parentCallback={handleCallback}
+            ></Child>
         </div>
     );
 }
