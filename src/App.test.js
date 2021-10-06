b73b2cd4874aa40cd6d238b94f7951ef8e7591d2
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-06 13:56:15 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -9,12 +9,24 @@ import { Provider } from 'react-redux';
 // guide: https://github.com/testing-library/jest-dom 
 // https://jestjs.io/docs/getting-started
 
+test('find eid in Emp', () => {
+  render(<Provider store={store}>
+    <Emp />
+  </Provider>);
+
+  expect(getByTestId(document.documentElement, 'eid')).toBeInTheDocument();
+});
+
 // sample test 1
 test('test 10 == 10', () => {
   expect(10).toBe(10);
 });
+
 // sample test 2
-test('test 11 == 11', () => {
-  expect(11).toBe(11);
+test('test 10 != 11', () => {
+  expect(10).not.toBe(11);
 });
 
+// Emp 
+
+// App 
